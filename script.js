daef4521e1fdc544bb0a2d0493764db7fb3eaae2
karinaferz <127227830+AnnaKarinaferreira@users.noreply.github.com>
2024-03-13 14:56:59 -0300

    up

---
+++ script.js
@@ -0,0 +1,31 @@
+// Referenciando o botão pelo ID
+var button3 = document.getElementById("button3");
+// Adicionando  clique ao botão
+button3.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "/subs/matematica.html";
+});
+
+// Referenciando o botão pelo ID
+var button4 = document.getElementById("button4");
+// Adicionando  clique ao botão
+button4.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "/subs/linguagens.html";
+});
+
+// Referenciando o botão pelo ID
+var button5 = document.getElementById("button5");
+// Adicionando  clique ao botão
+button5.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "/subs/humanas.html";
+});
+
+// Referenciando o botão pelo ID
+var button6 = document.getElementById("button6");
+// Adicionando  clique ao botão
+button6.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "/subs/natureza.html";
+});
