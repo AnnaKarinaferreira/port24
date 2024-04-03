02e3642c4c4e6c0113d9f87b95199ed75be4c864
karinaferz <127227830+AnnaKarinaferreira@users.noreply.github.com>
2024-04-03 12:45:03 -0300

    up

--- script.js
+++ script.js
@@ -35,26 +35,3 @@ var button7 = document.getElementById("button7");
 button7.addEventListener("click", function (){
   window.location.href = "/ti/geral.html";
 });
-
-var button8 = document.getElementById("button8");
-button8.addEventListener("click", function (){
-  window.location.href = "/ti/modelagem.html";
-});
-
-var button9 = document.getElementById("button9");
-button9.addEventListener("click", function (){
-  window.location.href = "/ti/implant.html";
-});
-
-var button10 = document.getElementById("button10");
-button10.addEventListener("click", function (){
-  window.location.href = "/ti/desen.html";
-});
-
-var button11 = document.getElementById("button11");
-button11.addEventListener("click", function (){
-  window.location.href = "/ti/situac.html";
-});
-
-
-
---
+++ subs/subs.js
@@ -0,0 +1,32 @@
+// Referenciando o botão pelo ID
+var vermais1 = document.getElementById("vermais1");
+// Adicionando  clique ao botão
+vermais1.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "https://www.canva.com/design/DAF-i5vZ_UA/mua0NbidbAYgPhyiBIuFyw/view?utm_content=DAF-i5vZ_UA&utm_campaign=designshare&utm_medium=link&utm_source=editor ";
+});
+
+// Referenciando o botão pelo ID
+var vermais2 = document.getElementById("vermais2");
+// Adicionando  clique ao botão
+vermais2.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "https://docs.google.com/document/d/1SyBCLdjUe-g1RhS-2EEJ9GP_Y2N_9-EjZZzBbj1Z0k4/edit?usp=sharing";
+});
+
+// Referenciando o botão pelo ID
+var vermais3 = document.getElementById("vermais3");
+// Adicionando  clique ao botão
+vermais3.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "https://www.canva.com/design/DAF9ipM_2VY/BSb6pMlFuRr6MKCdQW2sOQ/edit?utm_content=DAF9ipM_2VY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
+});
+
+// Referenciando o botão pelo ID
+var vermais4 = document.getElementById("vermais4");
+// Adicionando  clique ao botão
+vermais4.addEventListener("click", function () {
+  // Redirecionando
+  window.location.href = "https://docs.google.com/document/d/1lXP0J96P-L_LqDR8a8DKnB5D_4FY8wFt8coPDt-HqGQ/edit?usp=sharing";
+});
+
